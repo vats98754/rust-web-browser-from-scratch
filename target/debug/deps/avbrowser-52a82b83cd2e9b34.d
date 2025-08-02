6246d4d01a80c25f8e99428358af5791553c2b76
vats98754 <a7vats@uwaterloo.ca>
2025-08-03 04:38:03 +0530

    feat: successfully resolve all compilation errors and network issues - web browser now builds and runs correctly

---
+++ target/debug/deps/avbrowser-52a82b83cd2e9b34.d
@@ -0,0 +1,12 @@
+/Users/anvay-coder/rust-web-browser-from-scratch/target/debug/deps/avbrowser-52a82b83cd2e9b34.d: src/main.rs src/css.rs src/dom.rs src/html.rs src/layout.rs src/style.rs src/painting.rs src/pdf.rs
+
+/Users/anvay-coder/rust-web-browser-from-scratch/target/debug/deps/avbrowser-52a82b83cd2e9b34: src/main.rs src/css.rs src/dom.rs src/html.rs src/layout.rs src/style.rs src/painting.rs src/pdf.rs
+
+src/main.rs:
+src/css.rs:
+src/dom.rs:
+src/html.rs:
+src/layout.rs:
+src/style.rs:
+src/painting.rs:
+src/pdf.rs:
---
+++ target/debug/deps/avbrowser-9bd2fe5428e76747.d
@@ -0,0 +1,12 @@
+/Users/anvay-coder/rust-web-browser-from-scratch/target/debug/deps/avbrowser-9bd2fe5428e76747.d: src/main.rs src/css.rs src/dom.rs src/html.rs src/layout.rs src/style.rs src/painting.rs src/pdf.rs
+
+/Users/anvay-coder/rust-web-browser-from-scratch/target/debug/deps/libavbrowser-9bd2fe5428e76747.rmeta: src/main.rs src/css.rs src/dom.rs src/html.rs src/layout.rs src/style.rs src/painting.rs src/pdf.rs
+
+src/main.rs:
+src/css.rs:
+src/dom.rs:
+src/html.rs:
+src/layout.rs:
+src/style.rs:
+src/painting.rs:
+src/pdf.rs:
